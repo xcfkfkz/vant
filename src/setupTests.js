2456b7ce18d7a0cbe375b2f50933afc3bd76e977
xcfkfkz <49804610+xcfkfkz@users.noreply.github.com>
2021-11-28 14:45:21 +0800

    远程再提交一次

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,5 +1,5 @@
-import '@testing-library/jest-dom';
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
+import '@testing-library/jest-dom';
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
