0e7785b4412262302e302e1f16742fee3435d56b
xcfkfkz <xcfkfkz@hotmail.com>
2021-12-09 10:27:12 +0800

    dev提交3

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,6 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    // 主分支再3修改 我加几行注释 我加几行注释 在加几行注释
+    // 第三次加注释了 主分支再3修改 我加几行注释 我加几行注释 在加几行注释
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
