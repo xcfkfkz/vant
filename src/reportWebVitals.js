5d122bb9e5b83ebefc1f7c49896a65a5dd5fb2cc
xcfkfkz <xcfkfkz@hotmail.com>
2021-12-09 10:36:48 +0800

    master提交1

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,5 +1,6 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // 我在主分支上改了东西
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
